2d5fb822f9150ca7a2df4e6b17067920744ed49e
yebina1 <otqale01@gmail.com>
2026-05-10 03:47:15 +0900

    투표 수정 적용 (Layout, index 제외)

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react'
 import './Title.css'
 
 type TitleProps = {
-  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   title: ReactNode
   beforeTitle?: ReactNode
   children?: ReactNode
