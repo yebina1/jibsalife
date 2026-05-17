cce2cdfd5fb4747e6b58797f1908cf97bac4a362
yebina1 <otqale01@gmail.com>
2026-05-18 01:02:29 +0900

    챗봇 api 수정

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const genAI = new GoogleGenerativeAI(
 );
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-1.5-flash",
 });
 
 app.post("/api/chat", async (req, res) => {
